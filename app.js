6ed35f81d035150ac10bf44ea726fd246cdcad75
Aaron Lee <worldedit05@gmail.com>
2017-01-05 01:52:58 -0600

    Added args and a little code clean up

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const botCredentials = require("./config/config.js");
+const config = require("./config/config.js");
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
@@ -10,14 +10,24 @@ client.on('message', msg => {
   if (msg.author.bot) {
     return;
   }
-  if (msg.isMentioned('265614426340720640')) {
-    if (msg.content.startsWith('ping')) {
+  if (msg.isMentioned(config.botId)) {
+
+    let command = msg.content.split(" ")[1];
+    let args = msg.content.split(" ").slice(2);
+
+    if (command === 'ping') {
       msg.reply('Pong!');
     }
-    else if (msg.content.includes('hello')) {
+    else if (command === 'Hello') {
       msg.reply("Hey!");
+    }
+    else if (command === 'add') {
+      let numArray = args.map(n => parseInt(n));
+      let total = numArray.reduce( (p, c) => p+c);
+
+      msg.reply("The sum is " + total);
     }
   }
 });
 
-client.login(botCredentials.token);
+client.login(config.token);
